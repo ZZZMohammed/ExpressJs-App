cfb55e4c16e9e3a4a669b812017e36ae97a7a6ca
aouad <aouadmouhamed1@gmail.com>
2025-05-20 03:27:22 +0100

    hash the password with bcryptjs

--- Model/User.js
+++ Model/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,5 +30,20 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 }, { timestamps: true }); // 👈 adds createdAt and updatedAt automatically
+
+
+
+// hash the password
+userSchema.pre('save' , async function(next){
+    if(!this.isModified('password'))
+        return next();
+
+    const salt = await bcrypt.genSalt(10) ;
+    this.password = await bcrypt.hash(this.password , salt) ;
+    next();
+
+}) ;
+
+
 
 module.exports = mongoose.model('User', userSchema);
